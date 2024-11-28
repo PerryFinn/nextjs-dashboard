d804df372e09037692fac36ed952554979f53698
PerryFinn <perryfinn@163.com>
2024-11-28 23:01:54 +0800

    实现发票创建和编辑页面，添加客户数据获取，优化导航链接和按钮组件

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -24,21 +24,21 @@ export default function NavLinks() {
   const pathname = usePathname();
   return (
     <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
+      {links.map((item) => {
+        const LinkIcon = item.icon;
         return (
           <Link
-            key={link.name}
-            href={link.href}
+            key={item.name}
+            href={item.href}
             className={clsx(
               'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-sky-100 text-blue-600': pathname === item.href,
               }
             )}
           >
             <LinkIcon className='w-6' />
-            <p className='hidden md:block'>{link.name}</p>
+            <p className='hidden md:block'>{item.name}</p>
           </Link>
         );
       })}
