096159795fe90962b31d182328c68fa0f55f8403
PerryFinn <perryfinn@163.com>
2024-11-29 00:26:40 +0800

    增强发票创建表单的验证，添加错误处理和用户反馈；重构表单逻辑以支持状态管理

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,26 +1,48 @@
 'use server';
 
 import { sql } from '@vercel/postgres';
+import { error } from 'console';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: '请选择一个客户',
+  }),
+  amount: z.coerce.number().gt(0, '请输入一个大于0的金额'),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: '请选择一个发票状态',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(preState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { amount, customerId, status } = validatedFields.data;
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
