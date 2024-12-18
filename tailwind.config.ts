3404b8ae8e28f225c9ca92b0845a89efea577743
PerryFinn <perryfinn@163.com>
2024-12-18 23:29:00 +0800

    添加 NextUI 依赖并配置 Tailwind CSS，更新主页以使用 NextUIProvider 包裹组件

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,14 @@
 import type { Config } from 'tailwindcss';
+import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       gridTemplateColumns: {
@@ -27,6 +30,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [require('@tailwindcss/forms'), nextui()],
 };
 export default config;
